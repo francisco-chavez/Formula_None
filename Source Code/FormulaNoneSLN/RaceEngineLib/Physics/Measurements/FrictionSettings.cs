f6173c1fe342dd6ceea37eb3f7c9837151820edf
francisco-chavez <francisco.chavez.509@outlook.com>
2014-05-08 15:44:43 -0700

    Race Engine: Adding Friction Coefficient Table

---
+++ Source Code/FormulaNoneSLN/RaceEngineLib/Physics/Measurements/FrictionSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unv.RaceEngineLib.Physics.Measurements
+{
+	public static class FrictionSettings
+	{
+		public static readonly FrictionCoefficient RubberOnConcrete	= new FrictionCoefficient(1f, 0.8f, .02f);
+		public static readonly FrictionCoefficient SteelOnSteel		= new FrictionCoefficient(0.8f, 0.6f, 0.002f);
+
+		/// <summary>
+		/// I didn't find any concrete info for rubber on steel, so I 
+		/// averaged out the rubber on concrete and the steel on steel.
+		/// </summary>
+		public static readonly FrictionCoefficient RubberOnSteel	= new FrictionCoefficient(0.9f, 0.7f, 0.101f);
+
+
+		public struct FrictionCoefficient
+		{
+			public readonly float StaticFriction;
+			public readonly float KineticFriction;
+			public readonly float RollingFriction;
+
+			public FrictionCoefficient(float staticFriction, float kineticFriction, float rollingFriction)
+			{
+				StaticFriction = staticFriction;
+				KineticFriction = kineticFriction;
+				RollingFriction = rollingFriction;
+			}
+		}
+	}
+}
