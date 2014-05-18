58b565028a9bddcf6bdf38fde8723a6ee0bb4ab9
francisco-chavez <francisco.chavez.509@outlook.com>
2014-05-17 21:57:06 -0700

    Race Track Editor: Obstacle Systems

--- Source Code/FormulaNoneSLN/RaceTrackEditor/Views/ObstacleDataView.xaml.cs
+++ Source Code/FormulaNoneSLN/RaceTrackEditor/Views/ObstacleDataView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Unv.RaceTrackEditor.ViewModels;
+
 
 namespace Unv.RaceTrackEditor.Views
 {
