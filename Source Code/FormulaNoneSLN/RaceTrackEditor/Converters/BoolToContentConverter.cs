b5a172afc2b8dee8365a68c501a971841224c69b
francisco-chavez <francisco.chavez.509@outlook.com>
2014-05-19 17:52:28 -0700

    Race Track Editor: Obstacle System
    I've got the obstacles to glow on mouse over. I had to move the visual I
    use as a glow item out of the Obstacle View and into the template in the
    Obstacle Layer View that sets the Obstacle View as the view for the
    Obstacle View Model.

---
+++ Source Code/FormulaNoneSLN/RaceTrackEditor/Converters/BoolToContentConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace Unv.RaceTrackEditor.Converters
+{
+	public class BoolToContentConverter
+		: IValueConverter
+	{
+		public object TrueContent	{ get; set; }
+		public object FalseContent	{ get; set; }
+		public object NullContent	{ get; set; }
+
+
+		public BoolToContentConverter()
+		{
+			TrueContent		= "True";
+			FalseContent	= "False";
+			NullContent		= "Null";
+		}
+
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool? input = value as bool?;
+
+			if (value == null)
+				return NullContent;
+
+			return input.Value ? TrueContent : FalseContent;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
