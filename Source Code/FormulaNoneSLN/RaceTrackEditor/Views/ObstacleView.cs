06c3227f69cf77b2fef7b54880cd2881cbbbc2dc
francisco-chavez <francisco.chavez.509@outlook.com>
2014-05-19 23:19:06 -0700

    Race Track Editor: Obstacle System
    I've got the Obstacle Views being dragged and dropped by the Mouse.

--- Source Code/FormulaNoneSLN/RaceTrackEditor/Views/ObstacleView.cs
+++ Source Code/FormulaNoneSLN/RaceTrackEditor/Views/ObstacleView.cs
@@ -6,33 +6,71 @@ using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media.Imaging;
 
+using Unv.RaceTrackEditor.ViewModels;
+
 
 namespace Unv.RaceTrackEditor.Views
 {
 	public class ObstacleView
 		: Thumb
 	{
+		#region Attributes
 		public static readonly DependencyProperty ObstacleImageProperty;
+		public static readonly DependencyProperty ViewModelProperty;
+		#endregion
 
 
+		#region Properties
 		public BitmapImage ObstacleImage
 		{
 			get { return (BitmapImage) GetValue(ObstacleImageProperty); }
 			set { SetValue(ObstacleImageProperty, value); }
 		}
 
+		public ObstacleViewModel ViewModel
+		{
+			get { return (ObstacleViewModel) GetValue(ViewModelProperty); }
+			set { SetValue(ViewModelProperty, value); }
+		}
+		#endregion
 
+
+		#region Constructors
 		static ObstacleView()
 		{
 			DefaultStyleKeyProperty.OverrideMetadata(
 				typeof(ObstacleView),
 				new FrameworkPropertyMetadata(typeof(ObstacleView)));
 
+
 			ObstacleImageProperty = DependencyProperty.Register(
 				"ObstacleImage",
 				typeof(BitmapImage),
 				typeof(ObstacleView),
 				new FrameworkPropertyMetadata(null));
+
+			ViewModelProperty = DependencyProperty.Register(
+				"ViewModel",
+				typeof(ObstacleViewModel),
+				typeof(ObstacleView),
+				new FrameworkPropertyMetadata(
+					null, 
+					FrameworkPropertyMetadataOptions.AffectsParentArrange | FrameworkPropertyMetadataOptions.AffectsParentMeasure));
 		}
+
+		public ObstacleView()
+		{
+			this.DragDelta += new DragDeltaEventHandler(ObstacleView_DragDelta);
+		}
+		#endregion
+
+
+		#region Event Handlers
+		void ObstacleView_DragDelta(object sender, DragDeltaEventArgs e)
+		{
+			ViewModel.X += e.HorizontalChange;
+			ViewModel.Y += e.VerticalChange;
+		}
+		#endregion
 	}
 }
