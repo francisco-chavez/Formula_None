0a1a8f4a2497f0be8086d72bcc0551881047058a
francisco-chavez <francisco.chavez.509@outlook.com>
2014-05-19 22:43:15 -0700

    Race Track Editor: Obstacle System
    Switched the Obstacle View's base over to a Thumb class. This should
    help a bit with the mouse drag. It also let me move some of the Obstacle
    View's setup out of the Obstacle Layer View.

---
+++ Source Code/FormulaNoneSLN/RaceTrackEditor/Views/ObstacleView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Imaging;
+
+
+namespace Unv.RaceTrackEditor.Views
+{
+	public class ObstacleView
+		: Thumb
+	{
+		public static readonly DependencyProperty ObstacleImageProperty;
+
+
+		public BitmapImage ObstacleImage
+		{
+			get { return (BitmapImage) GetValue(ObstacleImageProperty); }
+			set { SetValue(ObstacleImageProperty, value); }
+		}
+
+
+		static ObstacleView()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(
+				typeof(ObstacleView),
+				new FrameworkPropertyMetadata(typeof(ObstacleView)));
+
+			ObstacleImageProperty = DependencyProperty.Register(
+				"ObstacleImage",
+				typeof(BitmapImage),
+				typeof(ObstacleView),
+				new FrameworkPropertyMetadata(null));
+		}
+	}
+}
