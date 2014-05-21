dd210ad0ec7bb5a6cad5658a3a07ceb7fb569b64
francisco-chavez <francisco.chavez.509@outlook.com>
2014-05-21 13:50:49 -0700

    Race Track Editor: Obstacle System
    Started work on a system for inserting new obstacles into the selected
    layer. I'm currently trying to use the adorner layer to draw out where
    the user is placing the new obstacles.

---
+++ Source Code/FormulaNoneSLN/RaceTrackEditor/Controls/ObstacleLayerViewAdorner.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+using Unv.RaceTrackEditor.Views;
+
+
+namespace Unv.RaceTrackEditor.Controls
+{
+	public class ObstacleLayerViewAdorner
+		: Adorner
+	{
+		#region Attributes
+		private MeasuredCanvas		m_drawArea;
+		private ObstacleLayerView	m_parentView;
+		#endregion
+
+
+		#region Properties
+		protected override int VisualChildrenCount
+		{
+			get { return 1; }
+		}
+		#endregion
+
+
+		#region Constructors
+		public ObstacleLayerViewAdorner(UIElement adornerElement)
+			: base(adornerElement)
+		{
+			if (!(adornerElement is ObstacleLayerView))
+				throw new ArgumentException();
+
+			m_parentView	= (ObstacleLayerView) adornerElement;
+			m_drawArea		= new MeasuredCanvas();
+		}
+		#endregion
+
+
+		#region Methods
+		protected override Visual GetVisualChild(int index)
+		{
+			return m_drawArea;
+		}
+
+		protected override Size MeasureOverride(Size constraint)
+		{
+			m_drawArea.Measure(constraint);
+			return m_drawArea.DesiredSize;
+		}
+
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			m_drawArea.Arrange(new Rect(finalSize));
+			return finalSize;
+		}
+		#endregion
+	}
+}
--- Source Code/FormulaNoneSLN/RaceTrackEditor/Views/ObstacleLayerView.xaml.cs
+++ Source Code/FormulaNoneSLN/RaceTrackEditor/Views/ObstacleLayerView.xaml.cs
@@ -12,17 +12,66 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Unv.RaceTrackEditor.Controls;
+
 
 namespace Unv.RaceTrackEditor.Views
 {
 	/// <summary>
 	/// Interaction logic for ObstacleLayerView.xaml
 	/// </summary>
-	public partial class ObstacleLayerView : UserControl
+	public partial class ObstacleLayerView 
+		: UserControl
 	{
+		#region Attributes
+		private Point			m_startingPosition;
+		private Point			m_currentPosition;
+
+		private ObstacleLayerViewAdorner	m_adorner;
+		private AdornerLayer				m_adornerLayer;
+		#endregion
+
+
+		#region Constructors
 		public ObstacleLayerView()
 		{
 			InitializeComponent();
+
+			m_adorner = new ObstacleLayerViewAdorner(this);
+			this.MouseLeftButtonDown += new MouseButtonEventHandler(ObstacleLayerView_MouseLeftButtonDown);
+			this.Loaded += new RoutedEventHandler(ObstacleLayerView_Loaded);
 		}
+		#endregion
+
+
+		#region Event Handlers
+		void ObstacleLayerView_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (m_adornerLayer == null)
+			{
+				m_adornerLayer = AdornerLayer.GetAdornerLayer(this);
+				m_adornerLayer.Add(m_adorner);
+			}
+		}
+
+		void ObstacleLayerView_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			this.MouseMove += new MouseEventHandler(ObstacleLayerView_MouseMove);
+			this.MouseLeftButtonUp += new MouseButtonEventHandler(ObstacleLayerView_MouseLeftButtonUp);
+
+			m_startingPosition = Mouse.GetPosition(this);
+			m_currentPosition = m_startingPosition;
+		}
+
+		void ObstacleLayerView_MouseMove(object sender, MouseEventArgs e)
+		{
+			m_currentPosition = Mouse.GetPosition(this);
+		}
+
+		void ObstacleLayerView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
 	}
 }
