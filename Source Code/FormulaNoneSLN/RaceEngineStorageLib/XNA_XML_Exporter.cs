0bb15a2366bb37e941d2a39a30ebe4621972b8df
francisco-chavez <francisco.chavez.509@outlook.com>
2014-05-26 12:56:03 -0700

    Race Engine Storage
    Added a class for exporting the race engine items to XML version that's
    used by XNA's content pipeline.

---
+++ Source Code/FormulaNoneSLN/RaceEngineStorageLib/XNA_XML_Exporter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Intermediate;
+
+
+namespace Unv.RaceEngineLib.Storage
+{
+	public class XNA_XML_Exporter
+	{
+		public static void WriteToFile(string filepath, object data)
+		{
+			XmlWriterSettings settings = new XmlWriterSettings();
+			settings.Indent = true;
+
+			using (XmlWriter writer = XmlWriter.Create(filepath, settings))
+			{
+				IntermediateSerializer.Serialize(writer, data, null);
+			}
+		}
+	}
+}
