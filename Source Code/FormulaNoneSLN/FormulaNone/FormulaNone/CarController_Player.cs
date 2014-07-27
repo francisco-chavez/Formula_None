8898057fe363a84b602a104055d8c3f17b6a90ca
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-26 20:47:10 -0700

    Mostly commenting some of the code.

--- Source Code/FormulaNoneSLN/FormulaNone/FormulaNone/CarController_Player.cs
+++ Source Code/FormulaNoneSLN/FormulaNone/FormulaNone/CarController_Player.cs
@@ -8,22 +8,55 @@ using Unv.RaceEngineLib.CarControl;
 
 namespace Unv.FormulaNone
 {
+	/// <summary>
+	/// This class converts the input from a GamePad, into input for a RaceCar instance.
+	/// </summary>
 	public class CarController_Player
 		: ICarController
 	{
 		#region Attributes
+		/// <summary>
+		/// This class pulls keeps track of the input from the game pads and keyboard.
+		/// </summary>
 		private InputState	m_input;
+
+		/// <summary>
+		/// This tells us which GamePad to pull our information from.
+		/// </summary>
 		private PlayerIndex m_playerIndex;
 		#endregion
 
 
 		#region Properties
+		/// <summary>
+		/// Gets the position of the Car's Steering Wheel. This should range from -1.0f to 1.0f.
+		/// </summary>
 		public float		SteeringPosition	{ get; private set; }
+
+		/// <summary>
+		/// Gets the position of the Gas Pedal. This should range from 0.0f to 1.0f.
+		/// </summary>
 		public float		GasPedalPosition	{ get; private set; }
+
+		/// <summary>
+		/// Gets the position of the Break Pedal. This shold range from 0.0f to 1.0f.
+		/// </summary>
 		public float		BreakPedalPosition	{ get; private set; }
+
+		/// <summary>
+		/// Gets whether or not the car is set to reverse.
+		/// </summary>
 		public bool			IsInReverse			{ get; private set; }
 
+		/// <summary>
+		/// Gets or sets the car that is supposed to be using these control settings.
+		/// </summary>
 		public RaceCar		RaceCar				{ get; set; }
+
+		/// <summary>
+		/// Gets or set the RaceEngine, that the RaceCar is suppose to be controlling
+		/// the RaceCar.
+		/// </summary>
 		public RaceEngine	RaceEngine			{ get; set; }
 		#endregion
 
@@ -31,21 +64,32 @@ namespace Unv.FormulaNone
 		#region Constructors
 		public CarController_Player(InputState input, PlayerIndex playerIndex)
 		{
-			m_input			= input;
-			m_playerIndex	= playerIndex;
+			this.m_input			= input;
+			this.m_playerIndex		= playerIndex;
+
+			this.BreakPedalPosition = 0f;
+			this.GasPedalPosition	= 0f;
+			this.IsInReverse		= false;
+			this.RaceCar			= null;
+
+			this.RaceEngine			= null;
+			this.SteeringPosition	= 0f;
 		}
 		#endregion
 
 
 		#region Methods
+		/// <summary>
+		/// Updates the control settings for the car.
+		/// </summary>
 		public void Update(float elapsedTimeMS)
 		{
 			var gamePadState = m_input.CurrentGamePadStates[(int) m_playerIndex];
 
-			this.BreakPedalPosition = gamePadState.Triggers.Left;
-			this.GasPedalPosition = gamePadState.Triggers.Right;
-			this.IsInReverse = gamePadState.Buttons.A == ButtonState.Pressed;
-			this.SteeringPosition = gamePadState.ThumbSticks.Left.X;
+			this.BreakPedalPosition = MathHelper.Clamp(gamePadState.Triggers.Left,		 0f, 1f);
+			this.GasPedalPosition	= MathHelper.Clamp(gamePadState.Triggers.Right,		 0f, 1f);
+			this.SteeringPosition	= MathHelper.Clamp(gamePadState.ThumbSticks.Left.X, -1f, 1f);
+			this.IsInReverse		= gamePadState.Buttons.A == ButtonState.Pressed;
 		}
 		#endregion
 	}
--- Source Code/FormulaNoneSLN/RaceEngineLib/CarControl/ICarController.cs
+++ Source Code/FormulaNoneSLN/RaceEngineLib/CarControl/ICarController.cs
@@ -8,15 +8,41 @@ namespace Unv.RaceEngineLib.CarControl
 {
 	public interface ICarController
 	{
+		/// <summary>
+		/// Gets the position of the Car's Steering Wheel. This should range from -1.0f to 1.0f.
+		/// </summary>
 		float	SteeringPosition	{ get; }
+
+		/// <summary>
+		/// Gets the position of the Gas Pedal. This should range from 0.0f to 1.0f.
+		/// </summary>
 		float	GasPedalPosition	{ get; }
+
+		/// <summary>
+		/// Gets the position of the Break Pedal. This shold range from 0.0f to 1.0f.
+		/// </summary>
 		float	BreakPedalPosition	{ get; }
+
+		/// <summary>
+		/// Gets whether or not the car is set to reverse.
+		/// </summary>
 		bool	IsInReverse			{ get; }
 
+		/// <summary>
+		/// Gets or sets the car that is supposed to be using these control settings.
+		/// </summary>
 		RaceCar		RaceCar		{ get; set; }
+
+		/// <summary>
+		/// Gets or set the RaceEngine, that the RaceCar is suppose to be controlling
+		/// the RaceCar.
+		/// </summary>
 		RaceEngine	RaceEngine	{ get; set; }
 
 
+		/// <summary>
+		/// Updates the control settings for the car.
+		/// </summary>
 		void Update(float elapsedTimeMS);
 	}
 }
