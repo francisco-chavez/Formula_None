e6189b263351d16e6819413acc1395951e47fc62
francisco-chavez <francisco.chavez.509@outlook.com>
2014-08-24 20:21:19 -0700

    I've stared working on fine collision detection.

--- Source Code/FormulaNoneSLN/RaceEngineLib/Physics/CollisionEngine.cs
+++ Source Code/FormulaNoneSLN/RaceEngineLib/Physics/CollisionEngine.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+
 using Unv.RaceEngineLib.Physics.Measurements;
+using Unv.RaceEngineLib.Physics.Shapes;
 
 
 namespace Unv.RaceEngineLib.Physics
@@ -13,7 +16,6 @@ namespace Unv.RaceEngineLib.Physics
 		#region Attributes
 		private List<Body> _mobileBodies	= null;
 		private List<Body> _immobileBodies	= null;
-		private List<Pair> _pairs			= null;
 		#endregion
 
 
@@ -36,7 +38,6 @@ namespace Unv.RaceEngineLib.Physics
 
 			_mobileBodies	= new List<Body>(mobileCapacity);
 			_immobileBodies = new List<Body>(immobileCapacity);
-			_pairs			= new List<Pair>(mobileCapacity);
 		}
 		#endregion
 
@@ -82,22 +83,36 @@ namespace Unv.RaceEngineLib.Physics
 
 		public void GenerateInitialCollisionData()
 		{
+			List<Pair>		needCloserLook	= new List<Pair>();
+			List<Impact>	impacts			= new List<Impact>();
+
 			for (int i = 0; i < _mobileBodies.Count; i++)
 			{
 				for (int j = 0; j < _immobileBodies.Count; j++)
 					if (ShouldLookCloser(_mobileBodies[i], _immobileBodies[j]))
-						_pairs.Add(new Pair() { A = _mobileBodies[i], B = _immobileBodies[j] });
+						needCloserLook.Add(new Pair() { A = _mobileBodies[i], B = _immobileBodies[j] });
 
 				for (int j = i + 1; j < _mobileBodies.Count; j++)
 					if (ShouldLookCloser(_mobileBodies[i], _mobileBodies[j]))
-						_pairs.Add(new Pair() { A = _mobileBodies[i], B = _mobileBodies[j] });
+						needCloserLook.Add(new Pair() { A = _mobileBodies[i], B = _mobileBodies[j] });
 			}
 
-			foreach (var pair in _pairs)
+			foreach (var pair in needCloserLook)
 			{
+				Impact impactOnA;
+				Impact impactOnB;
+
+				bool doTheyCollide = DoTheyCollide(pair.A, pair.B, out impactOnA, out impactOnB);
+
+				if (doTheyCollide)
+				{
+					impacts.Add(impactOnA);
+					impacts.Add(impactOnB);
+				}
 			}
 
-			_pairs.Clear();
+			needCloserLook.Clear();
+			impacts.Clear();
 		}
 
 		/// <summary>
@@ -115,6 +130,80 @@ namespace Unv.RaceEngineLib.Physics
 			bool lookCloser = distance2 < r2;
 
 			return lookCloser;
+		}
+
+		private bool DoTheyCollide(Body bodyA, Body bodyB, out Impact impactOnA, out Impact impactOnB)
+		{
+			///
+			/// I'm cheating a bit because I know:
+			/// * That all the cars use the same ConvexPolygon for their collision shape
+			/// * That the first body is always a car body
+			/// * That the second body is either a car or an obstacle
+			/// * That the obstacle body collision shape is always a Circular shape
+			/// * That the Circular shapes are immobile
+			if (bodyB.CollisionDetectionShape is Circular)
+				return DoTheyCollide(bodyA, (ConvexPolygon) bodyA.CollisionDetectionShape, out impactOnA, bodyB, (Circular) bodyB.CollisionDetectionShape, out impactOnB);
+			else if (bodyB.CollisionDetectionShape is ConvexPolygon)
+				return DoTheyCollide(bodyA, out impactOnA, bodyB, out impactOnB, (ConvexPolygon) bodyA.CollisionDetectionShape);
+
+			throw new ArgumentException();
+		}
+
+		private bool DoTheyCollide(Body bodyA, ConvexPolygon impactShapeA, out Impact impactOnA, Body bodyB, Circular impactShapeB, out Impact impactOnB)
+		{
+			impactOnA = new Impact();
+			impactOnB = new Impact();
+
+			Vector2[] bodyAPoints = new Vector2[impactShapeA.BorderPoints.Length];
+			impactShapeA.BorderPoints.CopyTo(bodyAPoints, 0);
+
+			for (int i = 0; i < bodyAPoints.Length; i++)
+			{
+				bodyAPoints[i] = bodyAPoints[i].Rotate(bodyA.Rotation);
+				bodyAPoints[i] = bodyAPoints[i] + bodyA.Position;
+			}
+
+			Vector2 axisNorm	= Vector2.UnitX;
+			float	minPen		= float.MaxValue;
+
+			for (int i = 0; i < bodyAPoints.Length; i++)
+			{
+				Vector2 point1 = bodyAPoints[i];
+				Vector2 point2 = bodyAPoints[(i + 1) % bodyAPoints.Length];
+				Vector2 normalAxis = point2 - point1;
+				normalAxis.Normalize();
+
+				float bExtentCenter = Vector2.Dot(normalAxis, bodyB.Position);
+
+				float aMax = float.MinValue;
+				float aMin = float.MaxValue;
+
+				float bMax = bExtentCenter + impactShapeB.Radius;
+				float bMin = bExtentCenter - impactShapeB.Radius;
+
+
+				for (int j = 0; i < bodyAPoints.Length; j++)
+				{
+					aMax = (float) Math.Max(aMax, Vector2.Dot(normalAxis, bodyAPoints[j]));
+					aMin = (float) Math.Min(aMin, Vector2.Dot(normalAxis, bodyAPoints[j]));
+				}
+
+				float aExtentCenter = (aMax + aMin) / 2f;
+
+				if (aMax < bMin)
+					return false;
+
+				float penAlongThisAxis = ((aMax - aMin) / 2f) + ((bMax - bMin) / 2f) - Math.Abs(bExtentCenter - aExtentCenter);
+
+				throw new NotImplementedException();
+			}
+
+			throw new NotImplementedException();
+		}
+
+		private bool DoTheyCollide(Body bodyA, out Impact impactOnA, Body bodyB, out Impact impactOnB, ConvexPolygon impactShape)
+		{
+			throw new NotImplementedException();
 		}
 		#endregion
 	}
